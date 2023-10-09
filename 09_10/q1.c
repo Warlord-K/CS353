93def562a5842272d9f8aafbac8ecfabb19dd194
Yatharth Gupta <95569637+Warlord-K@users.noreply.github.com>
2023-10-09 10:24:06 +0000

    Q1 with comments

--- 09_10/q1.c
+++ 09_10/q1.c
@@ -1,58 +1,47 @@
-/*
-
-Consider a system with one parent process and two child processes A and B.
-There is a shared signed integer X (assign value through user input). Process A
-increments X by one, 100000 times in a for loop. Process B decrements X by one,
-100000 times in a for loop. After both A and B finish, the parent process prints the
-final value of X.
-Hint: Declare a shared memory variable to hold X (using system calls shmget(),
-shmat(), shmdt(), and shmctl()). Write the programs for processes A and B. Do not
-put any synchronization code in the code for A and B. You should write the code in
-such a way so that you can simulate race condition in your program by slowing
-down A or B appropriately by using sleep() calls at appropriate points. Note that if
-there is no race condition, the value of X finally should be the one given through
-user input. Simulating race conditions means that if you run the program a few
-times, sometimes the final value of X printed by your program should be non-zero.
-
-
-*/
-
 #include <stdio.h>
 #include <stdlib.h>
 #include <sys/ipc.h>
 #include <sys/wait.h>
 #include <sys/types.h>
-#include <sys/sem.h>
 #include <sys/shm.h>
-
+#include <unistd.h>
 
 int main(){
+
+    // Declare the Required Variables: Shared Memory id, X, count and flag to ensure the parent process waits for the child processes to finish
     int shmID;
     int* X;
     int count  = 100000;
     int flag;
     
+    // Create a shared memory segment and attach it to the variable X
     shmID = shmget(IPC_PRIVATE, sizeof(int), 0666|IPC_CREAT);
     X = (int*)shmat(shmID, NULL, 0);
 
+    // Read the value of X from the user
     int n;
     printf("Enter value of X: ");
     scanf("%d", &n);
     *X = n;
 
+    // Create two child processes and increment and decrement the value of X in them
     if(fork() == 0){
+        // First Process
         while(count--){
             (*X)++;
         }
     }else{
         if(fork() == 0){
+            // Second Process
             while(count--){
                 (*X)--;
             }
         }else{
+            // Parent Process
             wait(&flag);
             wait(&flag);
-            printf("final value of X: %d\n", *X);
+            printf("Final Value of X: %d\n", *X);
+            // Detach and Delete the shared memory segment
             shmctl(shmID, IPC_RMID, NULL);
         }
     }
